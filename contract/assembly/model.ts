0534436bec1d79416408563d5f2624d442a8f3a8
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-08-14 16:12:59 +0400

    added checks and improved documentation

--- contract/assembly/model.ts
+++ contract/assembly/model.ts
@@ -1,4 +1,4 @@
-import { PersistentVector, u128, context, storage } from "near-sdk-core";
+import { PersistentVector, u128, context, storage } from "near-sdk-as";
 
 @nearBindgen //serializes custom class before storing it on the blockchain
 export class Coffee {
@@ -8,7 +8,7 @@ export class Coffee {
     message: string;
     amount: u128;
     timestamp: u64;
-    public static fromPayload(payload: Coffee): Coffee { //static method that takses a payload and returns a new Product object
+    public static fromPayload(payload: Coffee): Coffee { //static method that takes a payload and returns a new Coffee object
         const coffee = new Coffee();
         coffee.id = payload.id;
         coffee.giver = context.predecessor;
@@ -26,7 +26,10 @@ export const STORAGE_COST: u128 = u128.from("1000000000000000000000")
 
 export const AllCoffees = new PersistentVector<Coffee>("allCoffees")
 
-// Beneficiary
+/**
+ * @dev sets beneficiary
+ * @param beneficiary wallet id of beneficiary
+ */
 export function set_beneficiary(beneficiary: string): void {
     storage.set<string>("beneficiary", beneficiary)
 }
@@ -36,13 +39,18 @@ export function get_beneficiary(): string {
 }
 
 // Coffee Price
-// Default
+// Default = 1 NEAR
 const coffee_price: u128 = u128.from("1000000000000000000000000");
 
 export function set_coffee_price(price: u128): void {
     storage.set<u128>("price", price)
 }
 
+
+/**
+ * @notice if price hasn't been set in storage, the default coffee_price is returned
+ * @returns price of a coffee
+ */
 export function get_coffee_price(): u128 {
     if (!storage.contains("price")) { return coffee_price }
     return storage.getSome<u128>("price")
@@ -55,9 +63,7 @@ export function add_coffee(coffee: Coffee): i32 {
 }
 
 export function get_coffee(coffee_number: i32): Coffee {
-    assert(coffee_number > 0 && coffee_number <= AllCoffees.length,
-        "Error: Invalid Coffee number")
-    return AllCoffees[coffee_number - 1]
+    return AllCoffees[coffee_number];
 }
 
 export function total_coffees(): i32 {
